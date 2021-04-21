4f2c452c1e3019727ffe701ccb5a62485de92ddf
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-04-20 19:42:16 -0500

    HTML initial setup, javascript timer initial setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,26 @@ let DateTime = luxon.DateTime;
 var dtfull = DateTime.fromObject({zone: 'America/Los_Angeles', numberingSystem: 'beng'})
 var dt = DateTime.now();
 var interval15 = 15;
-var interval30 = 30;
+var interval30 = 3;
 var interval60 = 60;
 var timerDisplay = document.getElementById("timer");
 console.log(timerDisplay);
 
 
-var timerBtn = document.querySelector("#startTimer").addEventListener("click", function countdown(){
-    setInterval(() => {
-        timerDisplay.textContent = interval30;
-        interval30--;
+// var timerBtn = document.querySelector("#startStopTimer").addEventListener("click", function countdown(){
+//     setInterval(() => {
+//         timerDisplay.textContent = "0:" + interval30;
+//         interval30--;
+//     }, 1000);
+
+// });
+
+var timerBtn = document.querySelector("#startStopTimer").addEventListener("click", function countdown(){
+var timeInterval = setInterval(function set() {
+    timerDisplay.textContent = "0:" + interval30;
+    interval30--;
+    if(interval30 < 0) {
+        clearInterval(timeInterval);
+    }
     }, 1000);
 });
